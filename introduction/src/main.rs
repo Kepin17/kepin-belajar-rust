b1e6d8d88521163edcfe7f52d6200ac9a344e540
Kevien.OJ <69498060+Kepin17@users.noreply.github.com>
2024-08-24 20:56:06 +0700

    add:learn variable

--- introduction/src/main.rs
+++ introduction/src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
+
+    // print
     println!("Hello, world!");
+
+    // variable with mutable
+    let mut name= "Kepin";
+    println!("Hi, my name is {}", name);
+    name = "Jarvis";
+    println!("Hi, my name is {}", name);
+    
+    // variable with immutable
+    let name= "Kepin";
+    println!("Hi, my name is {}", name);
+    let name = "Jarvis";
+    println!("Hi, my name is {}", name);
+
+    let x = 5;
+    println!("The value of x is: {}", x);
+    {
+        let x =x +  3;
+        println!("The value of x is: {}", x);
+
+        let x = x + 1;
+        println!("The value of x is: {}", x);
+    }
+
+    let x = x + 1;
+    print!("The value of x is: {}", x);
+
+
+    // const value
+
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
